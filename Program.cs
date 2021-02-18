e1771d76bc0d961b21fccdb924354957db4fefb3
mHvNG <mail@mathijshoving.nl>
2021-02-18 13:46:46 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hashing {
+
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("A hashing algorithm for C# made by Mathijs Hoving...");
+            Console.WriteLine("Starting");
+        }
+    }
+}
